e398337c9765394216ebc4f103c58b58d44f60d2
Katherine <kat2chao@gmail.com>
2021-07-20 18:02:18 +0000

    events board

--- app.py
+++ app.py
@@ -3,25 +3,26 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-# from flask_pymongo import PyMongo
+from flask import redirect
+from flask_pymongo import PyMongo
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+# events = [
+#         {"event":"First Day of Classes", "date":"2019-08-21"},
+#         {"event":"Winter Break", "date":"2019-12-20"},
+#         {"event":"Finals Begin", "date":"2019-12-01"}
+#     ]
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
+app.config['MONGO_DBNAME'] = 'database'
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
+app.config['MONGO_URI'] = 'mongodb+srv://admin:kirkland2$@cluster0.bvl1d.mongodb.net/database?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
@@ -30,6 +31,8 @@ events = [
 @app.route('/index')
 
 def index():
+    events = mongo.db.events
+    events = events.find({})
     return render_template('index.html', events = events)
 
 
@@ -39,8 +42,28 @@ def index():
 
 def add():
     # connect to the database
-
+    events = mongo.db.events
+    # # insert new data
+    # events.insert({"event": "First Day of Class", "date": "2021-09-13"})
+    # # return a message to the user
+    # return "event added"
     # insert new data
-
+    events.insert({"event": "College Move In Day", "date": "2021-08-14"})
     # return a message to the user
-    return ""
+    return "event added!"
+
+@app.route("/events/new", methods = ["GET", "POST"])
+def new_event():
+    if request.method == "GET":
+        return render_template("newevent.html")
+    else:
+        event_name = request.form["event_name"]
+        event_date = request.form["event_date"]
+        user_name = request.form["user_name"]
+        events = mongo.db.events
+        events.insert({
+            "event": event_name,
+            "date": event_date,
+            "user": user_name
+        })
+        return redirect("/")
